ea9308795e1d1419831b5ed768758c87744b4e85
murata astusuke <kuroppems@gmail.com>
2016-12-26 22:06:03 +0900

    NewProject

---
+++ app/src/main/java/com/card/wiki/moyashi/wikicards/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.card.wiki.moyashi.wikicards
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
